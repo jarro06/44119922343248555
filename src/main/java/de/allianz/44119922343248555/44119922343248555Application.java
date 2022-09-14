1fd0b7026cad07d68e1a16b8dd0444b890e6c475
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-09-14 13:02:49 +0200

    Create maven project

---
+++ src/main/java/de/allianz/44119922343248555/44119922343248555Application.java
@@ -0,0 +1,13 @@
+package de.allianz.44119922343248555;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 44119922343248555Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(44119922343248555Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/44119922343248555/44119922343248555ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.44119922343248555;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 44119922343248555ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
